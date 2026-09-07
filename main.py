ac9317a888e4f36df9ba9899a4ae77876defae63
opred-max <opred@inbox.ru>
2026-09-07 12:58:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -243,7 +243,6 @@ def process_cash(message):
                 f"🪦 Конверт «Памятники»: **{monuments:,.0f} ₽**\n"
                 f"🛡️ Конверт «Стабфонд» (начислено): **{stabfond:,.0f} ₽**"
             )
-            
             markup = types.InlineKeyboardMarkup()
             btn = types.InlineKeyboardButton("Внесено в Стабфонд ✅", callback_data=f"add_{stabfond}_{holidays}")
             markup.add(btn)
