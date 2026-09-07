858dcf17ffa83b2d00358052f7d19095c141f2bb
opred-max <opred@inbox.ru>
2026-09-07 13:48:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -273,10 +273,8 @@ markup = types.InlineKeyboardMarkup()
 btn = types.InlineKeyboardButton("Внесено в Стабфонд ✅", callback_data=f"add_{stabfond}_{holidays}")
 markup.add(btn)
 bot.send_message(message.chat.id, report, reply_markup=markup, parse_mode='Markdown')
-            
-except Exception as e:
+    except Exception as e:
         bot.send_message(message.chat.id, "❌ Произошла ошибка при расчете.")
-
 
 =====================================================================
 
